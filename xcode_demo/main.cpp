0f0ac47af10afb31a6689fc5eac7155b7bc8a974
chadWanderscheid <chadwdesign@gmail.com>
2015-12-01 21:54:08 -0800

    Initial Commit

---
+++ xcode_demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xcode_demo
+//
+//  Created by Chad Wanderscheid on 12/1/15.
+//  Copyright © 2015 Chad Wanderscheid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
